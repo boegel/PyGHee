aff946d317b98704e4f5da0e0f3d2b538e1d4dc4
Kenneth Hoste <kenneth.hoste@ugent.be>
2022-02-27 18:05:42 +0100

    enhance issue comment test request to show how to extract information from raw request body

--- pyghee/main.py
+++ pyghee/main.py
@@ -16,16 +16,21 @@ from .utils import log
 
 class ExamplePyGHee(PyGHee):
 
-    def handle_create_event(self, request, log_file=None):
+    def handle_create_event(self, event_info, log_file=None):
         """
         Handle create event (new branch, for example).
         """
         log("create event handled!", log_file=log_file)
 
-    def handle_issue_comment_event(self, request, log_file=None):
+    def handle_issue_comment_event(self, event_info, log_file=None):
         """
         Handle adding/removing of comment in issue or PR.
         """
+        request_body = event_info['raw_request_body']
+        issue_url = request_body['issue']['url']
+        comment_author = request_body['comment']['user']['login']
+        comment_txt = request_body['comment']['body']
+        log("Comment posted in %s by @%s: %s" % (issue_url, comment_author, comment_txt))
         log("issue_comment event handled!", log_file=log_file)
 
 
--- tests/event_data.py
+++ tests/event_data.py
@@ -10,21 +10,41 @@ EVENT_TYPE_ISSUE_COMMENT = 'issue_comment'
 REQUEST_ID_001 = 'd3ed7694-8a6c-4008-a93f-b92aa86a95a8'
 TIMESTAMP_001 = '1645367007403'  # 2022-02-20T15:23:27
 
-CREATE_BRANCH_REQUEST = Request("".encode(),
-{
-    'Timestamp': TIMESTAMP_001,
-    'X-GitHub-Event': EVENT_TYPE_CREATE,
-    'X-Request-Id': REQUEST_ID_001,
-    'X-Hub-Signature': 'sha1=0123456789abcedf0123456789abcedf01234567',  # fake signature!
-}, {
-})
+CREATE_BRANCH_REQUEST = Request(
+    # request.data
+    "".encode(),
+    {
+        # request.headers
+        'Timestamp': TIMESTAMP_001,
+        'X-GitHub-Event': EVENT_TYPE_CREATE,
+        'X-Hub-Signature': 'sha1=0123456789abcedf0123456789abcedf01234567',  # fake signature!
+        'X-Request-Id': REQUEST_ID_001,
+    }, {
+        # request.json
+        'action': ACTION_CREATED,
+    },
+)
 
-ISSUE_COMMENT_CREATED_REQUEST = Request("".encode(),
-{
-    'Timestamp': TIMESTAMP_001,
-    'X-GitHub-Event': EVENT_TYPE_ISSUE_COMMENT,
-    'X-Request-Id': REQUEST_ID_001,
-    'X-Hub-Signature': 'sha1=0123456789abcedf0123456789abcedf01234567',  # fake signature!
-}, {
-    'action': ACTION_CREATED,
-})
+ISSUE_COMMENT_CREATED_REQUEST = Request(
+    # request.data
+    "".encode(),
+    {
+        # request.headers
+        'Timestamp': TIMESTAMP_001,
+        'X-GitHub-Event': EVENT_TYPE_ISSUE_COMMENT,
+        'X-Hub-Signature': 'sha1=0123456789abcedf0123456789abcedf01234567',  # fake signature!
+        'X-Request-Id': REQUEST_ID_001,
+    }, {
+        # request.json
+        'action': ACTION_CREATED,
+        'comment': {
+            'body': "This is just a test",
+            'user': {
+                'login': 'boegel',
+            },
+        },
+        'issue': {
+            'url': 'https://github.com/boegel/PyGHee/issues/123456789',
+        },
+    },
+)
